0dc41c2859cf11dd2064911f5bd5e1e0e608de8f
Ilya <iachernikov1@gmail.com>
2022-03-22 14:36:29 +0300

    the solution gor createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathInArray = path.split('.');
 
+  return function (obj) {
+    let value = obj;
+
+    for (const key of pathInArray) {
+      if (value !== undefined) {
+        value = value[key];
+      } else {
+        break;
+      }
+    }
+
+    return value;
+  };
 }
+
