746a3ed4a4341c2212403874aa554f533b191669
Ilya <iachernikov1@gmail.com>
2022-04-13 14:47:15 +0300

    table v3 + form v1 without events

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -3,18 +3,273 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class SortableTable {
-  constructor(headersConfig, {
+  onArrowLink = null
+  start = 0
+  end = CHUNK_VALUE
+  isLoading = false
+  handleScrollEvent = async (event) =>{
+    if (this.isLoading) return;
+
+    const relativeBottom = document.documentElement.getBoundingClientRect().bottom;
+    if (relativeBottom < Number(document.documentElement.clientHeight) + 100) {
+      this.isLoading = true;
+      this.start += CHUNK_VALUE;
+      this.end += CHUNK_VALUE;
+      const responseData = await this.fetchData();
+      this._addData(responseData);
+      this.isLoading = false;
+    }
+
+  }
+
+  constructor(headerConfig, {
     data = [],
-    sorted = {}
+    sorted = {id: 'title', order: 'asc'},
+    isSortLocally = false,
+    url
   } = {}) {
+    this.headerConfig = headerConfig;
+    this.parseConfig();
+    this.isSortLocally = isSortLocally;
+    this.defaultSort = sorted;
+    this.render();
+    this.initEventListeners();
+
+    if (url) {
+      this.url = BACKEND_URL + '/' + url;
+      this.isLoading = true;
+      this.fetchData().then(data => this._replaceData(data));
+    } else {
+      this.elementBodyLink.parentElement.classList.add('sortable-table_loading');
+      this.data = data;
+      this.elementBodyLink.innerHTML = this.getTemplateTableBody(this.data);
+      this.elementBodyLink.parentElement.classList.remove('sortable-table_loading');
+    }
 
   }
 
-  sortOnClient (id, order) {
+  render () {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.elementBodyLink = element.querySelector('.sortable-table__body');
+    this.element = element.firstElementChild;
+  }
+
+  initEventListeners () {
+    if (document.readyState !== 'complete') {
+      document.addEventListener('DOMContentLoaded', this._initEvents.bind(this));
+    } else {
+      this._initEvents();
+    }
 
   }
 
-  sortOnServer (id, order) {
+  _initEvents () {
+    if (!this.onArrowLink) {
+      this.onArrowLink = document.querySelector(`[data-id=${this.defaultSort.id}] .sortable-table__sort-arrow`);
+      if (this.onArrowLink) {
+        this.onArrowLink.style.opacity = 1;
+      } else {
+        this.onArrowLink = document.querySelector('.sortable-table__sort-arrow');
+      }
+
+    }
+
+    this.element.querySelector('.sortable-table__header').addEventListener('pointerdown', this.handleSortEvent.bind(this));
+    window.addEventListener('scroll', this.handleScrollEvent);
 
   }
+
+
+
+  handleSortEvent (event) {
+    const sorTablCell = event.target.closest('.sortable-table__cell');
+    if (!sorTablCell) return;
+    if (sorTablCell.dataset.sortable === 'false') return;
+
+    this.sort(sorTablCell.dataset.id, sorTablCell.dataset.order);
+    if (sorTablCell.dataset.order === 'asc') {
+      sorTablCell.dataset.order = 'desc';
+    } else if (sorTablCell.dataset.order === 'desc') {
+      sorTablCell.dataset.order = 'asc';
+    }
+
+    const newArrowLink = sorTablCell.querySelector('.sortable-table__sort-arrow');
+    if (newArrowLink !== this.onArrowLink) {newArrowLink.style.opacity = 1;
+      this.onArrowLink.style.opacity = 0;
+      this.onArrowLink = newArrowLink;
+    }
+
+
+
+
+
+
+
+  }
+
+  sort (fieldValue = this.defaultSort.id, orderValue = this.defaultSort.order, isSortLocally = this.isSortLocally) {
+
+    if (isSortLocally) {
+      this.sortOnClient(fieldValue, orderValue);
+    } else {
+      this.sortOnServer(fieldValue, orderValue);
+      this.defaultSort.id = fieldValue;
+      this.defaultSort.order = orderValue;
+    }
+
+  }
+
+  sortOnClient (fieldValue, orderValue) {
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+
+    const direction = directions[orderValue];
+    this.elementBodyLink.innerHTML = this.getTemplateTableBody([...this.data].sort((a, b) =>{
+      return direction * this.sortByType(this.getSortType(fieldValue), a[fieldValue], b[fieldValue]);
+    }));
+  }
+
+  sortOnServer (fieldValue, orderValue) {
+    this.start = 0;
+    this.end = CHUNK_VALUE;
+    this.fetchData(orderValue, fieldValue).then(data => this._replaceData(data));
+  }
+
+
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+    this.elementBodyLink = null;
+  }
+
+  getTemplate () {
+    return `
+    <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+            <div data-element="header" class="sortable-table__header sortable-table__row">
+                ${this.getTemplateTableHeader()}
+            </div>
+
+            <div data-element="body" class="sortable-table__body">
+
+            </div>
+        </div>
+     </div>
+    `;
+  }
+
+  getTemplateTableHeader () {
+    return this.headerConfig.map(item => {
+      return `
+        <div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}" data-order=${this.defaultSort.order || 'asc'}>
+            <span>${item.title}</span>
+            ${this.getArrowSpan(item.sortable)}
+        </div>
+      `;
+    }).join('');
+  }
+
+  getTemplateTableBody (sortedData) {
+    return sortedData.map(item =>{
+      return `
+        <a href="/products/${item.id}" class="sortable-table__row">
+          ${this.headerConfig.map(itemConf => {
+        return itemConf.template(item);
+      }).join('')}
+        </a>
+      `;
+    }).join('');
+  }
+
+  getSortType (field) {
+    for (const item of this.headerConfig) {
+      if (item.id === field) {
+        if (item.sortable) {
+          return item.sortType;
+        }
+      }
+    }
+
+  }
+
+  sortByType (type, a, b) {
+    if (type === 'number') {return a - b;}
+    if (type === 'string') {return a.localeCompare(b, ['ru', 'en'], {'caseFirst': 'upper'});}
+  }
+
+  getDefaultTemplate (itemObj) {
+    return `<div class="sortable-table__cell">${itemObj[this.id]}</div>`;
+  }
+
+  parseConfig () {
+    for (const confEl of this.headerConfig) {
+      if (!confEl.template) {
+        confEl.template = this.getDefaultTemplate;
+      }
+    }
+
+  }
+
+  getArrowSpan (sortable) {
+    if (sortable) {
+      return `
+        <span data-element="arrow" class="sortable-table__sort-arrow" style="opacity: 0">
+          <span class="sort-arrow"></span>
+        </span>
+      `;
+    }
+    else {
+      return '';
+    }
+  }
+
+  fetchData (order = this.defaultSort.order, field = this.defaultSort.id) {
+    this.elementBodyLink.parentElement.classList.add('sortable-table_loading');
+    const url =  new URL(this.url);
+    url.searchParams.set('_start', this.start);
+    url.searchParams.set('_end', this.end);
+    url.searchParams.set('_order', order);
+    url.searchParams.set('_sort', field);
+    return fetchJson(url);
+  }
+
+  update (...args) {
+    // this.element.querySelector('.column-chart').classList.add('column-chart_loading');
+    if (!args.length) {
+      this._fetchData();
+      return;
+    }
+    if (args[0] instanceof Array) {
+      this._addData(args[0]);
+      return;
+    }
+    if (args[0] instanceof Date) {
+      this.fetchData(...args);
+    }
+
+  }
+
+  _addData (dataArray) {
+    // this.element.querySelector('.column-chart').classList.remove('column-chart_loading');
+    this.elementBodyLink.insertAdjacentHTML('beforeend', this.getTemplateTableBody(dataArray));
+    this.elementBodyLink.parentElement.classList.remove('sortable-table_loading');
+  }
+
+  _replaceData (dataArray) {
+    this.elementBodyLink.innerHTML = this.getTemplateTableBody(dataArray);
+    this.isLoading = false;
+    this.elementBodyLink.parentElement.classList.remove('sortable-table_loading');
+  }
+
+
 }
+
+const CHUNK_VALUE = 30;
--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -1,5 +1,6 @@
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
+// import {stat} from "@babel/core/lib/gensync-utils/fs";
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
@@ -10,6 +11,122 @@ export default class ProductForm {
   }
 
   async render () {
+    const div = document.createElement('div');
+    let productData = {};
 
+    if (this.productId) {
+      productData = await fetch(`${BACKEND_URL}/api/rest/products?id=${this.productId}`).then(resp => resp.json());
+      productData = productData[0];
+    }
+    div.innerHTML = await this.getTemplate(productData);
+    this.element = div.firstElementChild;
+  }
+
+  getImagesTemplate (imagesArray) {
+    if (!imagesArray) return '';
+
+    return imagesArray.map(item =>{
+      return `
+        <li class="products-edit__imagelist-item sortable-list__item">
+          <input type="hidden" name="url" value="${item.url}">
+          <input type="hidden" name="source" value="${item.source}">
+          <span>
+            <img src="/assets/icons/icon-grab.svg" data-grab-handle="" alt="grab">
+            <img class="sortable-table__cell-img" alt="Image" src="${item.url}">
+            <span>${item.source}</span>
+          </span>
+          <button type="button">
+            <img src="/assets/icons/icon-trash.svg" data-delete-handle="" alt="delete">
+          </button>
+      </li>
+      `;
+    }).join('');
+  }
+
+  async getCategotiesTemplate () {
+    const response = await fetch(`${BACKEND_URL}/api/rest/categories?_sort=weight&_refs=subcategory`).then(resp => resp.json());
+    return response.map(item => {
+      return item.subcategories.map(subcategory => {
+        return `
+           <option value="${subcategory.id}">${item.title} &gt; ${subcategory.title}</option>
+        `;
+      }).join('');
+    }).join('');
+  }
+
+  getProductStatus (status) {
+    if (status) {
+      return `
+        <option selected value="1">Активен</option>
+        <option value="0">Неактивен</option>
+      `;
+    } else {
+      return `
+        <option value="1">Активен</option>
+        <option selected value="0">Неактивен</option>
+      `;
+    }
+  }
+
+  async getTemplate(productData) {
+    return `
+      <div class="product-form">
+    <form data-element="productForm" class="form-grid">
+      <div class="form-group form-group__half_left">
+        <fieldset>
+          <label class="form-label">Название товара</label>
+          <input required="" type="text" name="title" class="form-control" placeholder="Название товара" value ='${productData.title || ''}'>  </fieldset>
+      </div>
+      <div class="form-group form-group__wide">
+        <label class="form-label">Описание</label>
+        <textarea required="" class="form-control" name="description" data-element="productDescription" placeholder="Описание товара">${productData.description || ''}</textarea>
+      </div>
+      <div class="form-group form-group__wide" data-element="sortable-list-container">
+        <label class="form-label">Фото</label>
+        <div data-element="imageListContainer">
+          <ul class="sortable-list">
+           ${this.getImagesTemplate(productData.images)}
+          </ul>
+        </div>
+        <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
+      </div>
+      <div class="form-group form-group__half_left">
+        <label class="form-label">Категория</label>
+        <select class="form-control" name="subcategory">
+            ${await this.getCategotiesTemplate()}
+        </select>
+      </div>
+      <div class="form-group form-group__half_left form-group__two-col">
+        <fieldset>
+          <label class="form-label">Цена ($)</label>
+          <input required="" type="number" name="price" class="form-control" placeholder="100" value="${productData.price || ''}"> </fieldset>
+        <fieldset>
+          <label class="form-label">Скидка ($)</label>
+          <input required="" type="number" name="discount" class="form-control" placeholder="0" value="${productData.discount || ''}"> </fieldset>
+      </div>
+      <div class="form-group form-group__part-half">
+        <label class="form-label">Количество</label>
+        <input required="" type="number" class="form-control" name="quantity" placeholder="1" value="${productData.quantity || ''}"> </div>
+      <div class="form-group form-group__part-half">
+        <label class="form-label">Статус</label>
+        <select class="form-control" name="status">
+          ${this.getProductStatus(productData.status)}
+        </select>
+      </div>
+      <div class="form-buttons">
+        <button type="submit" name="save" class="button-primary-outline"> Сохранить товар </button>
+      </div>
+    </form>
+  </div>
+    `;
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+  }
+
+  remove () {
+    this.element.remove();
   }
 }
